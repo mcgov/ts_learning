cd1d867552940c86db64457249721d4e958b3544
Matthew McGovern <mmcgover@oswego.edu>
2014-09-06 22:15:54 -0400

    Added No-Choice-Random 	No choice, but random.

--- No-Choice-Random.py
+++ No-Choice-Random.py
@@ -101,7 +101,7 @@ def present_no_choice_single(images, targetidx):
 						finished = True
 
 def present_no_choice_double(images, rightid, wrongid, order):
-	#print order
+	
 	guys = [None ,rightid, wrongid]
 	img = [None] * 3
 	totalclicks = -1
@@ -489,65 +489,81 @@ quadruple_displayat= [ ((screen.get_width()/4) + 90, 400), ((screen.get_width()/
 octuple_displayat =[ ((screen.get_width()/4) + OCTUPLE_OFFSET, 400-OCTUPLE_OFFSET), ((screen.get_width()/4) + OCTUPLE_OFFSET, 400+OCTUPLE_OFFSET),  ((screen.get_width()/4)-OCTUPLE_OFFSET, 400-OCTUPLE_OFFSET), ((screen.get_width()/4)-OCTUPLE_OFFSET, 400+OCTUPLE_OFFSET),  (((screen.get_width()/4)*3)+OCTUPLE_OFFSET, 400-OCTUPLE_OFFSET) , (((screen.get_width()/4)*3)+OCTUPLE_OFFSET, 400+OCTUPLE_OFFSET), (((screen.get_width()/4)*3)-OCTUPLE_OFFSET, 400-OCTUPLE_OFFSET), (((screen.get_width()/4)*3)-OCTUPLE_OFFSET, 400+OCTUPLE_OFFSET) ]
 ##present a number of blocks
 
-if len( sys.argv ) < 2:
-	sys.stderr.write("Error: No input filename specified\n")
-else:
-	with open(sys.argv[1], 'rb') as f:
-		## A csv.reader object is created with the file object f and named 'reader'. The delimiter parameter is actually optional.
-		## the reader will default to use the comma as a delimiter. There are more parameters that offer more control over how .csv files are opened.
-		## check out python's documentation of the csv class for more info on that.
-		reader =  csv.reader(f, delimiter=",")
-		
-		## then we can iterate through the reader without too much fuss. Just like running a for loop, we iterate through the reader object.
-		## it returns lists of all the things in each row. We can pick things from the list using the array notation [].
-		for row in reader:
-			print row  ## uncomment to check out how the reader parses .csv files. They are taken in as lists.
-			if row[0] == 'single':
-				print filename(target_images[int(row[1])]), present_no_choice_single(target_images, int(row[1]))
-			## so easy!
-			if row[0] == 'double':
-				parsed_order = [
-				int(row[3][1], 16),
-				int(row[3][2], 16),
-				int(row[3][3], 16),
-				int(row[3][4], 16),
-				int(row[3][5], 16),
-				int(row[3][6], 16) ]
-				print filename(target_images[int(row[1])]),filename(target_images[int(row[2])]), present_no_choice_double(target_images, int(row[1]), int(row[2]), parsed_order)
-			if row[0] == 'quad':
-				parsed_order = [
-				int(row[5][1], 16),
-				int(row[5][2], 16),
-				int(row[5][3], 16),
-				int(row[5][4], 16),
-				int(row[5][5], 16),
-				int(row[5][6], 16),
-				int(row[5][7], 16),
-				int(row[5][8], 16),
-				int(row[5][9], 16),
-				int(row[5][10], 16),
-				int(row[5][11], 16),
-				int(row[5][12], 16)
-				]
-				print filename(target_images[int(row[1])]), filename(target_images[int(row[2])]), filename(target_images[int(row[3])]), filename(target_images[int(row[4])]),
-				print present_no_choice_quadruple(target_images, int(row[1]), int(row[2]), int(row[3]), int(row[4]), parsed_order)
-			def generate_oct_order():
-				parsed_order = []
-				for i in range(1,25):
+#!# Can use random.sample
+def pickrandom(number):
+	numbers = []
+	picks = 0
+	while picks < number:
+		pick = randint(0,(len(target_images)-1))
+		if pick not in numbers:
+			#print pick
+			numbers.append(pick)
+			picks = picks + 1
+	return numbers
 
-					parsed_order.append( int(row[9][i], 16) )
-				
-				print filename(target_images[int(row[1])]), 
-				filename(target_images[int(row[2])]), 
-				filename(target_images[int(row[3])]), 
-				filename(target_images[int(row[4])]), 
-				filename(target_images[int(row[5])]), 
-				filename(target_images[int(row[6])]), 
-				filename(target_images[int(row[7])]) , 
-				filename(target_images[int(row[8])])
-				print present_no_choice_octuple(target_images, int(row[1]), int(row[2]), int(row[3]), int(row[4]), int(row[5]), int(row[6]), int(row[7]), int(row[8]), parsed_order)
-				### No choice octuple requires a 10-tuple, the array of all images to be used, the index of the images to be displayed (8), and a sequence of numbers that represents the order in which to display them, numbers 1-8
+seeds = pickrandom(15)
+targetidx = randint(0,(len(target_images)-1))
+# print "CHOICE SINGLES:"
+print present_no_choice_single(target_images, seeds[0])
 
+# print "CHOICE DOUBLES:" #################!# Can use random.sample
+def pickrandom(number):
+	numbers = []
+	picks = 0
+	while picks < number:
+		pick = randint(0,(len(target_images)-1))
+		if pick not in numbers:
+			#print pick
+			numbers.append(pick)
+			picks = picks + 1
+	return numbers
+
+for block in range(1):	  ########## NOTE: You can adjust the amount of times this runs a single by adjusting the integer in the parenthesis there <<< --------
+	 ## this is that function above ^^ to pick some non-repeating random integers
+	shuffle(double_displayat)
+	#print block, filename(target_images[seeds[0]]),filename(target_images[seeds[1]])
+	order = []
+	for i in range( 0, 3):
+		order.append(  seeds[1]  )
+		order.append(  seeds[2] )
+		shuffle( order )
+	print present_no_choice_double(target_images, seeds[1], seeds[2], order )
+
+# print "CHOICE QUADRUPLES:" ################################33
+for block in range(1):	########## NOTE: You can adjust the amount of times this runs  by adjusting the integer in the parenthesis there <<< --------
+	
+	 #pick some random images to display (by picking random indexes which we'll use to pull things from the array of image locations).
+	shuffle(quadruple_displayat)
+	order = []
+	for i in range( 0, 3):
+		order.append( seeds[3] )
+		order.append(  seeds[4] )
+		order.append( seeds[5] )
+		order.append( seeds[6] )
+		shuffle( order )
+	#print block, filename(target_images[seeds[0]]),filename(target_images[seeds[1]]), filename(target_images[seeds[2]]), filename(target_images[seeds[3]])
+	print present_no_choice_quadruple(target_images, seeds[3], seeds[4], seeds[5] ,seeds[6], order )
+	#!# Or: print present_choice_quadruple(target_images, *seeds)
+
+# print "CHOICE OCTUPLES:"  ################################
+for block in range (1):     ########## NOTE: You can adjust the amount of times this runs by adjusting the integer in the parenthesis there <<< --------
+	
+	shuffle (octuple_displayat)
+	order = []
+	for i in range( 0, 3):
+		order.append( seeds[7]  )
+		order.append(  seeds[8]  )
+		order.append(  seeds[9] )
+		order.append(  seeds[10] )
+		order.append(  seeds[11] )
+		order.append(  seeds[12]  )
+		order.append(  seeds[13]  )
+		order.append( seeds[14]  )
+		shuffle( order )
+	#print block, filename(target_images[seeds[0]]),filename(target_images[seeds[1]]), filename(target_images[seeds[2]]), filename(target_images[seeds[3]]), filename(target_images[seeds[4]]), filename(target_images[seeds[5]]), filename(target_images[seeds[6]]), filename(target_images[seeds[7]]),
+	print present_no_choice_octuple(target_images, seeds[7], seeds[8], seeds[9] ,seeds[10] ,seeds[11] ,seeds[12], seeds[13], seeds[14], order )
+
+			
 
 
 
